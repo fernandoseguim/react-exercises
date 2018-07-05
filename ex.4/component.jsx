6c52f619758db0d259dbc03d0ec66588e26259d4
Fernando Seguim <fernando.seguim@gmail.com>
2018-07-05 00:23:46 -0300

    export many components from file

---
+++ ex.4/component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FirstComponent = props => (
+    <h1>First component!</h1>
+)
+
+const SecondComponent = props => (
+    <h1>Second component!</h1>
+)
+
+export { FirstComponent, SecondComponent }
---
+++ ex.4/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { FirstComponent, SecondComponent } from './component'
+
+ReactDOM.render(
+    <div>
+        <FirstComponent />
+        <SecondComponent />
+    </div>
+, document.getElementById('app'))
