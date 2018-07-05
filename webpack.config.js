fd65443a64b71e51b6f22093b7e4571bbd7fc4cf
Fernando Seguim <fernando.seguim@gmail.com>
2018-07-04 22:55:26 -0300

    add webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const webpack = require('webpack');
+
+const path = require('path');
+
+module.exports = {
+    entry: './ex/index.jsx',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: './bundle.jsx'
+    },
+    devServer: {
+        port: 8080,
+        contentBase: './public'
+    },
+    module: {
+        loaders: [{
+            test: /.jsx?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ['es2015', 'react']
+            }
+        }]
+    }
+}
